3675350423dba29a8393984ac2a523c95c030083
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-03-05 14:16:01 -0500

    Add card contianer which maps an array of card objects to card components

---
+++ memory-game/src/components/CardContainer.jsx
@@ -0,0 +1,19 @@
+import "../styles/CardContainer.css";
+import Card from "./Card";
+
+export default function CardContainer({ cards, cardOnClick }) {
+  return (
+    <div className="card-container">
+      {cards.map((card) => {
+        return (
+          <Card
+            key={card.id}
+            name={card.name}
+            img={card.img}
+            onClick={(e) => cardOnClick(e, card.id)}
+          ></Card>
+        );
+      })}
+    </div>
+  );
+}
